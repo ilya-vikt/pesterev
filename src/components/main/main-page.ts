076ed76d2f7fa0a63fb337d963fbf61135570562
Ilya Viktorov <ilya-vikt@yandex.ru>
2023-05-03 22:41:42 +0300

    downgrade swiper from v9 to v8 (there was a bug with jumps slider when loop mode is active in v9 )

--- src/components/main/main-page.ts
+++ src/components/main/main-page.ts
@@ -1,5 +1,16 @@
-import { register } from 'swiper/element/bundle';
+import Swiper, { Navigation } from 'swiper';
+import type { SwiperOptions } from 'swiper/types';
 
 export const mainInit = () => {
-  register();
+  const swiperConfig: SwiperOptions = {
+    slidesPerView: 1,
+    navigation: {
+      prevEl: '.slider-btns__prev',
+      nextEl: '.slider-btns__next',
+    },
+    loop: true,
+  };
+
+  Swiper.use([Navigation]);
+  new Swiper('.main-hero__slider', swiperConfig);
 };
