bd7fd39e9353e2cf744c3b78de6425f65a49ffc2
Ilya Viktorov <ilya-vikt@yandex.ru>
2023-05-10 12:35:30 +0300

    add blur and backdrop animation of the menu

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,7 +1,36 @@
 const menu: HTMLElement = document.getElementById('menu');
 const menuBtn: HTMLElement = document.getElementById('menu-btn');
-const mainBlock: HTMLElement = document.getElementById('main');
+const backdropSupport = CSS.supports('backdrop-filter', 'blur()');
+const blocksToBlur = [...document.querySelectorAll('.need-blur')];
 let menuActive = false;
+
+function animateBlur(element: HTMLElement, direction: 'in' | 'out'): void {
+  let start = performance.now();
+  const duration = 300;
+
+  function step(timestamp: number): void {
+    let progress = (timestamp - start) / duration;
+    if (progress > 1) progress = 1;
+
+    let blurValue: number;
+    if (direction === 'in') {
+      blurValue = easeOut(progress) * 50;
+    } else {
+      blurValue = (1 - easeOut(progress)) * 50;
+    }
+    element.style.backdropFilter = `blur(${blurValue}px)`;
+
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  }
+
+  window.requestAnimationFrame(step);
+}
+
+function easeOut(progress: number): number {
+  return Math.pow(--progress, 5) + 1;
+}
 
 const toggleMenu = () => {
   const noScrollClass = 'no-scroll';
@@ -11,14 +40,24 @@ const toggleMenu = () => {
 
   if (menuActive) {
     document.body.classList.remove(noScrollClass);
-    mainBlock.classList.remove(blurClass);
     menu.classList.remove(menuActiveClass);
     menuBtn.classList.remove(btnActiveClass);
+
+    if (!backdropSupport) {
+      blocksToBlur.forEach((block) => block.classList.remove(blurClass));
+    } else {
+      animateBlur(menu, 'out');
+    }
   } else {
     document.body.classList.add(noScrollClass);
-    mainBlock.classList.add(blurClass);
     menu.classList.add(menuActiveClass);
     menuBtn.classList.add(btnActiveClass);
+
+    if (!backdropSupport) {
+      blocksToBlur.forEach((block) => block.classList.add(blurClass));
+    } else {
+      animateBlur(menu, 'in');
+    }
   }
   menuActive = !menuActive;
 };
--- src/components/main/main-page.ts
+++ src/components/main/main-page.ts
@@ -13,4 +13,16 @@ export const mainInit = () => {
 
   Swiper.use([Navigation]);
   new Swiper('.main-hero__slider', swiperConfig);
+
+  document
+    .querySelector('.main-hero__menu')
+    ?.addEventListener('click', ({ target }) => {
+      if ((target as HTMLElement).tagName !== 'A') return;
+      document
+        .querySelector('.main-hero__item--active')
+        ?.classList.remove('main-hero__item--active');
+      (target as HTMLElement)
+        .closest('.main-hero__item')
+        .classList.add('main-hero__item--active');
+    });
 };
