b9d85251752830960b73456f2c9fc50d7ab43e50
Ilya Viktorov <ilya-vikt@yandex.ru>
2023-05-25 15:41:11 +0300

    add the preloader and start animation

---
+++ src/components/preloader/preloader.ts
@@ -0,0 +1,27 @@
+function removePreloader() {
+  const preloader = document.getElementById('preloader');
+  if (!preloader) return;
+
+  setTimeout(() => {
+    document.body.classList.remove('no-scroll');
+    document.body.classList.add('start-animation');
+    preloader.remove();
+
+    setTimeout(() => {
+      document.body.classList.add('animating');
+    }, 50);
+
+    setTimeout(() => {
+      document.body.classList.remove('start-animation', 'animating');
+    }, 5000);
+  }, 500);
+}
+
+export function preloaderInit() {
+  if (document.readyState === 'complete') {
+    removePreloader();
+    return;
+  }
+
+  window.addEventListener('load', removePreloader, { once: true });
+}
--- src/ts/global-init.ts
+++ src/ts/global-init.ts
@@ -1,7 +1,9 @@
 import { scrollBarWidth } from '@ts/modules/functions';
 import { headerInit } from '@/components/header/header';
+import { preloaderInit } from '@/components/preloader/preloader';
 
 export const globalInit = (): void => {
   scrollBarWidth();
   headerInit();
+  preloaderInit();
 };
