a7b4e72584ac76cf6ece9cd473ad776bb856bc3b
Ilya Viktorov <ilya-vikt@yandex.ru>
2023-06-20 14:19:30 +0300

    remake: filter panel

--- src/components/share/aside-panel/aside-panel.ts
+++ src/components/share/aside-panel/aside-panel.ts
@@ -23,6 +23,19 @@ export const asidePanelInit = (): void => {
   panel = document.getElementById('aside-panel') as HTMLDivElement;
 
   if (!panel) return;
+
+  const topBlock = panel.querySelector('.aside-panel__top') as HTMLDivElement;
+
+  const handleResize = () => {
+    panel.style.setProperty('--top-panel-height', `${topBlock.offsetHeight}px`);
+  };
+
+  handleResize();
+
+  if (topBlock) {
+    new ResizeObserver(handleResize).observe(topBlock);
+  }
+
   panel.addEventListener('click', (e) => {
     if ((e.target as HTMLElement).classList.contains('close-trigger')) {
       closePanel();
