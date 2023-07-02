f1b788e37788d52e9a1abb7a165d9e297c295c92
Ilya Viktorov <ilya-vikt@yandex.ru>
2023-07-02 21:28:01 +0300

    add send-message api

--- src/api/send-message.ts
+++ src/api/send-message.ts
@@ -1,7 +1,21 @@
+type TCommunicatorConfig = {
+  url: string;
+  success: string;
+  unsuccess: string;
+};
+
+declare global {
+  interface Window {
+    communicatorConfig: TCommunicatorConfig;
+  }
+}
+
 export interface IMsg {
   name: string;
   email: string;
   message: string;
+  city: string;
+  _wpnonce: string;
 }
 
 export type TMsgResult = {
@@ -9,16 +23,21 @@ export type TMsgResult = {
   msg: string;
 };
 
-export const sendMessage = async (order: IMsg): Promise<TMsgResult> => {
-  return new Promise((resolve) => {
-    const res = {
-      success: true,
-      msg: `<p>Сообщение отправлено</p>
-      <p>Если по какой то причине вы не получили ответ в течение дня, пожалуйста, напишите в WhatsApp.</p>`,
-    };
-
-    setTimeout(() => {
-      resolve(res);
-    }, 2000);
-  });
+export const sendMessage = async (message: IMsg): Promise<TMsgResult> => {
+  let result;
+  try {
+    const response = await fetch(window.communicatorConfig.url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify(message),
+    });
+    result = response.ok;
+  } catch {
+    result = false;
+  }
+  return result
+    ? { success: true, msg: window.communicatorConfig.success }
+    : { success: false, msg: window.communicatorConfig.unsuccess };
 };
