c7f8edb9f5c8d7c6059f8309c9fbcaefbf4db7e0
Ilya Viktorov <ilya-vikt@yandex.ru>
2023-07-02 21:33:41 +0300

    add slider gallery on the product page

--- src/components/product/goods/goods.ts
+++ src/components/product/goods/goods.ts
@@ -1,8 +1,13 @@
 import { showPanel } from '@/components/share/aside-panel/aside-panel';
+import { createLightBox } from '@/libs/lightbox/lightbox';
 import { initSlider } from '@/ts/slider-init';
 
 export const goodsInit = () => {
-  initSlider('#product-slider');
+  const slider = initSlider('#product-slider');
+  document.getElementById('product-slider').addEventListener('click', () => {
+    const sliderData = window.galleries['slider'];
+    sliderData && createLightBox(sliderData, slider.activeIndex - 1);
+  });
   document
     .querySelectorAll('.buy-btn')
     ?.forEach((btn) => btn.addEventListener('click', showPanel));
